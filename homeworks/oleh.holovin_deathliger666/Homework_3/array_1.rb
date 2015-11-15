c79f358d12db2a4b6ca914f6bb874f48962db434
deathliger666 <oleg.golovin13@gmail.com>
2015-11-15 14:20:20 +0200

    removed folder

--- homeworks/oleh.holovin_deathliger666/Homework_3/array_1.rb
+++
@@ -1,9 +0,0 @@
-class Array1
-
-  cars = [ "Audi", "Corvette", "Volvo", "MAN","Lada", "Daewoo", "Volks Wagen", "Subaru"]
-
-  puts "#{cars.sort}"
-  puts "#{cars.reverse}"
-  puts "#{cars.shuffle}"
-
-end
