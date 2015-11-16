b6960bc735a475ac7eecdf41c653aa6e1e0c4269
IH47 <infinitehorizon47@mail.ru>
2015-11-17 01:03:18 +0200

    Update item.rb

--- homeworks/yura.smagliy_IH47/Homework3/item.rb
+++ homeworks/yura.smagliy_IH47/Homework3/item.rb
@@ -1,15 +1,15 @@
 class Item
 
-	attr_accessor :price
-	attr_reader :weight
+  attr_accessor :price
+  attr_reader :weight
 
-	def initialize(options = {})
-		@price = options[:price] ? options[:price] : 0
-	 	@weight = options[:weight] ? options[:weight] : 0
-	end
+  def initialize(options = {})
+    @price = options[:price] ? options[:price] : 0
+    @weight = options[:weight] ? options[:weight] : 0
+  end
 
-	def show_info
-		puts "Item - price: #{@price}, weight: #{@weight}"
-	end
+  def show_info
+    puts "Item - price: #{@price}, weight: #{@weight}"
+  end
 
 end
