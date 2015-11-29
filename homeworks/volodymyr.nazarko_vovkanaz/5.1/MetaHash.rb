0dea65914980193b8bd88a8b13fedc8557fccecd
vovkanaz <vovkanaz@gmail.com>
2015-11-29 14:27:01 +0200

    MetaHash fixed

--- homeworks/volodymyr.nazarko_vovkanaz/5.1/MetaHash.rb
+++ homeworks/volodymyr.nazarko_vovkanaz/5.1/MetaHash.rb
@@ -11,22 +11,42 @@ MyHash='{"person":{
                     }
           }'
 
+
 pars = JSON.parse(MyHash)
+
 
 module Info
 
   module InfoMethods
 
+
+
     def info_methods
 
+
       define_method :social_profiles_size do
-        puts social_profiles.size
+          puts social_profiles.size
       end
 
+       define_method :adult? do
+        personal_data['age'] < 18
+      end
+
+      define_method :get_snake_name do
+          additional_info["pets"].find {|i| i["species"] == "snake"}["name"]
+      end
+        
+      define_method  :twit_account do
+          social_profiles.join.scan(/vk.ru/).any?
+      end
+      
+      define_method :add_new_hobby do |arg1='Girls', arg2='Lisa'|
+          additional_info.merge({arg1=>[arg2]})
+      end
 
     end
-
   end
+
 
   def self.included(base)
     base.extend(InfoMethods)
@@ -34,50 +54,31 @@ module Info
 
 end
 
-
 User = Struct.new(*pars['person'].keys.collect(&:to_sym)) do
-
- def little?
-   if personal_data['age'] <= 18
-     puts "Teenager"
-   else
-     puts "Student"
-   end
- end
-
 end
-
-
 
 human = User.new(*pars["person"].values)
 
 human.class.class_eval do
   include Info
   info_methods
-  define_method :get_snake_name do
-    additional_info["pets"].find {|i| i["species"] == "snake"}["name"]
-  end
-end
-
-
-class << human
-  def gender
-   if personal_data['gender'] == 'male'
-     puts "Man"
-   elsif personal_data['gender'] == 'female'
-     puts "Woman"
-   else
-     puts "Something else"
-   end
-  end
 end
 
 
 puts "Count of social_profiles"
 human.social_profiles_size
-puts "Who is our  user"
-human.little?
+p "#############################"
+puts "Does she adult?"
+puts human.adult?
+p "#############################"
 puts "What's name of snake:"
 puts human.get_snake_name
-puts "Our user is:"
-puts human.gender
+p "#############################"
+puts "Does she have account in vk?"
+puts human.twit_account
+p "#############################"
+puts "Adding new hobby"
+puts human.add_new_hobby("Sport","Football")
+puts "Last register in":
+puts human.social_profiles.last
+
